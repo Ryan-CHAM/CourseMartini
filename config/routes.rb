26717ebeb34b7983d8461625c8d992ecc7728696
Kongouuu <2301788091@qq.com>
2020-03-19 20:43:11 +0800

    added a welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+  get 'welcome' => 'welcome#index', :as => "welcome"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'account#signup'
   get 'account/signup'
   get 'account/signin'
   get 'admin/user_table'

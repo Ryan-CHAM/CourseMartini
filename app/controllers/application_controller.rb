df2a57f97f8741c9d4ca526e6aeec1ee8c8ab5a9
tchsieh <tchsieh9@cse.cuhk.edu.hk>
2020-03-27 20:50:33 +0800

    welcome page update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,18 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery
     before_action :configure_permitted_parameters, if: :devise_controller?
-    before_action :first_time_visit, unless: -> { cookies[:first_visit] }
 
     protected
     
-    def first_time_visit
-		cookies.permanent[:first_visit] = 1
-      	@first_visit = true
+    def first_time_visiting?
+      if cookies[:first_time].nil?
+        cookies[:first_time] = true
+        true
+      else
+        false
+      end
     end
+
     def configure_permitted_parameters
       	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end

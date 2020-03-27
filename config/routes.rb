4b26e11786f0c0c7177c69ed851b49ecf72b4918
Jinan Zhou <jinan.zhou@link.cuhk.edu.hk>
2020-03-27 14:49:18 +0800

    Revert "Search Function"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :courses
-  get "/search", to: "courses#search"
-  resources :comments
+	resources :comments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

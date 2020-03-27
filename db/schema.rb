132b58bfd043c5197127f04d5e826747a31ec6fa
DeeperMind <deeper-mind@outlook.com>
2020-03-27 16:47:58 +0800

    reslove the conflict in user model

--- db/schema.rb
+++ db/schema.rb
@@ -57,12 +57,4 @@ ActiveRecord::Schema.define(version: 2020_03_26_091326) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end

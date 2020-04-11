d7c49206924afab280a36735ec6293926e269e3c
tchsieh <tchsieh9@cse.cuhk.edu.hk>
2020-04-11 21:08:32 +0800

    Fixed first visit action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,14 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery
     before_action :configure_permitted_parameters, if: :devise_controller?
-
+    before_action :first_time_visit?, unless: -> { cookies[:first_visit] }
     protected
     
-    def first_time_visiting?
-      if cookies[:first_time].nil?
-        cookies[:first_time] = true
-        true
-      else
-        false
-      end
-    end
+
+  def first_time_visit?
+     cookies[:first_visit] = 1
+     redirect_to '/welcome'
+  end
 
     def configure_permitted_parameters
       	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])

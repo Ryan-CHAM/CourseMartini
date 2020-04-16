aa23439aa54bb0025dae5490dc6aaefd6dacb4e8
DeeperMind <deeper-mind@outlook.com>
2020-04-17 02:01:44 +0800

    add instructor to course

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_125327) do
+ActiveRecord::Schema.define(version: 2020_04_16_173006) do
 
   create_table "comments", force: :cascade do |t|
     t.text "posts"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_04_15_125327) do
     t.text "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "instructor"
   end
 
   create_table "users", force: :cascade do |t|

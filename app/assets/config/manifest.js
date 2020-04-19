21db6090a2c2636460779fbbb6f283db5e12a80c
Kongouuu <2301788091@qq.com>
2020-04-19 19:26:10 +0800

    bug fix

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,10 +1,2 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= require jquery
-//= require jquery-ui
-//= require popper
-//= require bootstrap-sprockets
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .

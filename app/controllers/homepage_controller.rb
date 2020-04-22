e6197e97c5d61e7605a5fc982e786f451f58a07d
Ryan CHAM <1155124230@link.cuhk.edu.hk>
2020-04-22 22:31:47 +0800

    Stupid HUANG Zihao

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -21,4 +21,24 @@ class HomepageController < ApplicationController
       @sesruoc = yarra.to_json.html_safe
   end
   
+  def overview
+      array = Array.new
+      array[0] = ['Course', 'Workload', 'TeachingQuality', 'Faculty', 'Overall']
+      i = 1
+      Course.all.each do |course|
+          array[i] = [course.code, course.workload, course.quality, course.faculty, course.overall]
+          i = i + 1
+      end
+      @courses = array.to_json.html_safe
+      
+      yarra = Array.new
+      yarra[0] = ['Course Code', 'Teaching Qulity', 'Workload', 'Usefulness', 'Difficulty', 'CGPA', 'Overall']
+      i = 1
+      Course.all.each do |course|
+          yarra[i] = [course.code, course.quality, course.workload, course.usefulness, course.difficulty, course.gpa, course.overall]
+          i = i + 1
+      end
+      @sesruoc = yarra.to_json.html_safe
+  end
+  
 end

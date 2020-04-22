1d3cc9ad76856db21857d66e3da9af59fae9be9e
Ryan CHAM <1155124230@link.cuhk.edu.hk>
2020-04-22 23:12:36 +0800

    Stupid Me

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,25 +1,5 @@
 class HomepageController < ApplicationController
   include Devise::Controllers::Helpers 
-
-  def index
-      array = Array.new
-      array[0] = ['Course', 'Workload', 'TeachingQuality', 'Faculty', 'Overall']
-      i = 1
-      Course.all.each do |course|
-          array[i] = [course.code, course.workload, course.quality, course.faculty, course.overall]
-          i = i + 1
-      end
-      @courses = array.to_json.html_safe
-      
-      yarra = Array.new
-      yarra[0] = ['Course Code', 'Teaching Qulity', 'Workload', 'Usefulness', 'Difficulty', 'CGPA', 'Overall']
-      i = 1
-      Course.all.each do |course|
-          yarra[i] = [course.code, course.quality, course.workload, course.usefulness, course.difficulty, course.gpa, course.overall]
-          i = i + 1
-      end
-      @sesruoc = yarra.to_json.html_safe
-  end
   
   def overview
       array = Array.new
